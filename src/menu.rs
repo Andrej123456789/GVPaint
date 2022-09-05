81ffe0b61df4623054bdb1f55b238891c4466b15
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-09-05 17:13:37 +0200

    Refactoring and basic block painting

--- src/menu.rs
+++ src/menu.rs
@@ -3,13 +3,30 @@ use crossterm::{terminal, style, cursor, QueueableCommand};
 use termios::{Termios, TCSANOW, ECHO, ICANON, tcsetattr};
 
 static mut ALREADY: bool = false;
+
 enum KEY {
     ERROR,
     W,
     S,
     A,
     D,
-    ENTER
+    PLACE,
+    ERASE
+}
+
+enum COLOR {
+    BLACK,
+    GREY,
+    RED,
+    GREEN,
+    BLUE,
+    MAGENTA,
+    LGREEN,
+    AQUA,
+    YELLOW,
+    ORANGE,
+    BROWN,
+    WHITE
 }
 
 /// Reads a character from user input. If multiple characters are given,
@@ -35,7 +52,7 @@ fn read_user_input_character() -> char {
     return buffer[0] as char;
 }
 
-fn cursor_input() ->  u32 {
+fn cursor_input() -> u32 {
     let ch = read_user_input_character();
 
     unsafe {
@@ -51,16 +68,58 @@ fn cursor_input() ->  u32 {
 
     let ch_u32: u32 = ch as u32;
 
-    match (ch_u32) {
-        119 => return KEY::W as u32,
-        115 => return KEY::S as u32,
-        97 => return KEY::A as u32,
-        100 => return KEY::D as u32,
-        0 => return KEY::ENTER as u32,
+    match ch_u32 {
+        119 | 87 => return KEY::W as u32,
+        115 | 83 => return KEY::S as u32,
+        97  | 65 => return KEY::A as u32,
+        100 | 68 => return KEY::D as u32,
+        113 | 81 => return KEY::PLACE as u32,
+        101 | 69 => return KEY::ERASE as u32,
         _ => return KEY::ERROR as u32,
     }
+}
 
-    return KEY::ERROR as u32;
+fn get_color(stdout: &mut Stdout) -> u32 {
+    return COLOR::RED as u32; /* all colors will be implemented soon, hopefully */
+}
+
+fn remove_old_cursor(stdout: &mut Stdout, cursor_x: f64, cursor_y: f64) {
+    stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
+    stdout.queue(style::SetForegroundColor(style::Color::White));
+    println!("\u{2588}");
+}
+
+fn place_new_cursor(stdout: &mut Stdout, cursor_x: f64, cursor_y: f64) {
+    stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
+    stdout.queue(style::SetForegroundColor(style::Color::Black));
+    println!("\u{2588}");
+}
+
+fn place_blok(stdout: &mut Stdout, cursor_x: f64, cursor_y: f64, color: u32) {
+    let mut crossterm_color = style::Color::Black;
+
+    match color {
+        0 => crossterm_color =  style::Color::Black,
+        1 => crossterm_color =  style::Color::Grey,
+        2 => crossterm_color =  style::Color::Red,
+        3 => crossterm_color =  style::Color::Green,
+        4 => crossterm_color =  style::Color::Blue,
+        5 => crossterm_color =  style::Color::Magenta,
+        6 => crossterm_color =  style::Color::Rgb{r: 14, g: 237, b: 22},
+        7 => crossterm_color =  style::Color::Rgb{r: 24, g: 194, b: 137},
+        8 => crossterm_color =  style::Color::Yellow,
+        9 => crossterm_color =  style::Color::Rgb{r: 237, g: 116, b: 24},
+        10 => crossterm_color =  style::Color::Rgb{r: 102, g: 0, b: 0},
+        11 => crossterm_color = style::Color::White,
+        _ => crossterm_color = style::Color::Black
+    }
+
+    stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
+    stdout.queue(style::SetForegroundColor(crossterm_color));
+    println!("\u{2588}");
+
+    cursor_x -= 1.0;
+    place_new_cursor(&mut stdout, cursor_x, cursor_y);
 }
 
 fn logic(width: u16, height:u16) {
@@ -82,47 +141,35 @@ fn logic(width: u16, height:u16) {
         let key: u32 = cursor_input();
         
         if key == 1 {
-            stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
-            stdout.queue(style::SetForegroundColor(style::Color::White));
-            println!("\u{2588}");
-
+            remove_old_cursor(&mut stdout, cursor_x, cursor_y);
             cursor_y -= 1.0;
-            stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
-            stdout.queue(style::SetForegroundColor(style::Color::Black));
-            println!("\u{2588}");
+            place_new_cursor(&mut stdout, cursor_x, cursor_y);
         }
     
         else if key == 2 {
-            stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
-            stdout.queue(style::SetForegroundColor(style::Color::White));
-            println!("\u{2588}");
-
+            remove_old_cursor(&mut stdout, cursor_x, cursor_y);
             cursor_y += 1.0;
-            stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
-            stdout.queue(style::SetForegroundColor(style::Color::Black));
-            println!("\u{2588}");
+            place_new_cursor(&mut stdout, cursor_x, cursor_y);
         }
     
         else if key == 3 {
-            stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
-            stdout.queue(style::SetForegroundColor(style::Color::White));
-            println!("\u{2588}");
-
+            remove_old_cursor(&mut stdout, cursor_x, cursor_y);
             cursor_x -= 1.0;
-            stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
-            stdout.queue(style::SetForegroundColor(style::Color::Black));
-            println!("\u{2588}");
+            place_new_cursor(&mut stdout, cursor_x, cursor_y);
         }
     
         else if key == 4 {
-            stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
-            stdout.queue(style::SetForegroundColor(style::Color::White));
-            println!("\u{2588}");
-            
+            remove_old_cursor(&mut stdout, cursor_x, cursor_y);
             cursor_x += 1.0;
-            stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16)); 
-            stdout.queue(style::SetForegroundColor(style::Color::Black));
-            println!("\u{2588}");
+            place_new_cursor(&mut stdout, cursor_x, cursor_y);
+        }
+
+        else if key == 5 { /* PLACE */
+            place_blok(&mut stdout, cursor_x, cursor_y, 2); /* all colors will be implemented soon, hopefully */
+        }
+
+        else if key == 6 { /* ERASE */
+            place_blok(&mut stdout, cursor_x, cursor_y, 11);
         }
     
         else {
