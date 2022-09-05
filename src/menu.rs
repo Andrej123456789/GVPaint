68cbe120b3b822c9192fc97b756bbee6823b56a1
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-09-05 20:57:26 +0200

    Don't override a painting

--- src/menu.rs
+++ src/menu.rs
@@ -1,4 +1,5 @@
-use std::{{io, io::{Read, Write, Stdout, stdout}}, thread, time::Duration};
+use std::{{io, io::{Read, Write, Stdout, stdout}}};
+use std::collections::BTreeMap;
 use crossterm::{terminal, style, cursor, QueueableCommand};
 use termios::{Termios, TCSANOW, ECHO, ICANON, tcsetattr};
 
@@ -89,13 +90,39 @@ fn get_color(stdout: &mut Stdout) -> u32 {
     return COLOR::RED as u32; /* all colors will be implemented soon, hopefully */
 }
 
-fn remove_old_cursor(stdout: &mut Stdout, cursor_x: f64, cursor_y: f64) {
+fn remove_old_cursor(stdout: &mut Stdout, cursor_x: f64, cursor_y: f64, placed: &mut BTreeMap<(u32, u32), u32>) {
     stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
     stdout.queue(style::SetForegroundColor(style::Color::White));
     println!("\u{2588}");
+
+    for (k, v) in placed {
+        if (cursor_x as u32) == k.0 { /* we can just check any axis, x or y */
+            stdout.queue(cursor::MoveTo(k.0 as u16, k.1 as u16));
+
+            let mut crossterm_color = style::Color::Black;
+            match v {
+                0 => crossterm_color =  style::Color::Black,
+                1 => crossterm_color =  style::Color::Grey,
+                2 => crossterm_color =  style::Color::Red,
+                3 => crossterm_color =  style::Color::Green,
+                4 => crossterm_color =  style::Color::Blue,
+                5 => crossterm_color =  style::Color::Magenta,
+                6 => crossterm_color =  style::Color::Rgb{r: 14, g: 237, b: 22},
+                7 => crossterm_color =  style::Color::Rgb{r: 24, g: 194, b: 137},
+                8 => crossterm_color =  style::Color::Yellow,
+                9 => crossterm_color =  style::Color::Rgb{r: 237, g: 116, b: 24},
+                10 => crossterm_color =  style::Color::Rgb{r: 102, g: 0, b: 0},
+                11 => crossterm_color = style::Color::White,
+                _ => crossterm_color = style::Color::Black
+            }
+
+            stdout.queue(style::SetForegroundColor(crossterm_color));
+            println!("\u{2588}");
+        }
+    }
 }
 
-fn place_new_cursor(stdout: &mut Stdout, mut cursor_x: f64, mut cursor_y: f64, height: u16) -> f64 {
+fn place_new_cursor(stdout: &mut Stdout, mut cursor_x: f64, mut cursor_y: f64, placed: &mut BTreeMap<(u32, u32), u32>, height: u16) -> f64 {
     if (cursor_y as u16) != (height - 2) {
         stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
         stdout.queue(style::SetForegroundColor(style::Color::Black));
@@ -103,16 +130,16 @@ fn place_new_cursor(stdout: &mut Stdout, mut cursor_x: f64, mut cursor_y: f64, h
     }
 
     else {
-        remove_old_cursor(stdout, cursor_x, cursor_y);
+        remove_old_cursor(stdout, cursor_x, cursor_y, placed);
 
         cursor_y -= 2.0;
-        cursor_y = place_new_cursor(stdout, cursor_x, cursor_y, height);
+        cursor_y = place_new_cursor(stdout, cursor_x, cursor_y, placed, height);
     }
 
     return cursor_y;
 }
 
-fn place_blok(stdout: &mut Stdout, cursor_x: f64, cursor_y: f64, color: u32) {
+fn place_blok(stdout: &mut Stdout, cursor_x: f64, cursor_y: f64, placed: &mut BTreeMap<(u32, u32), u32>, color: u32) {
     let mut crossterm_color = style::Color::Black;
 
     match color {
@@ -131,6 +158,8 @@ fn place_blok(stdout: &mut Stdout, cursor_x: f64, cursor_y: f64, color: u32) {
         _ => crossterm_color = style::Color::Black
     }
 
+    placed.insert((cursor_x as u32, cursor_y as u32), color);
+
     stdout.queue(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
     stdout.queue(style::SetForegroundColor(crossterm_color));
     println!("\u{2588}");
@@ -141,6 +170,18 @@ fn logic(width: u16, height: u16) {
 
     let mut cursor_x: f64 = (width as f64) / (2.2 as f64);
     let mut cursor_y: f64 = (height as f64) / (2.2 as f64);
+
+    /*
+     * Shows which positions on screen are already taken so we can draw it again where cursor moves
+     * @param K - X axis (u32)
+     * @param V - Y axis (u32)
+    */
+    let mut placed: BTreeMap<(u32, u32), u32> = BTreeMap::new();
+    let mut i = 0;
+
+    for i in (0..16) {
+        placed.insert((i as u32, 0), 3);
+    }
 
     stdout.queue(style::SetForegroundColor(style::Color::Red));
     stdout.queue(cursor::MoveTo(0, 0));
@@ -153,57 +194,30 @@ fn logic(width: u16, height: u16) {
 
     loop {
         let key: u32 = cursor_input();
-        
-        if key == 1 {
-            remove_old_cursor(&mut stdout, cursor_x, cursor_y);
-            cursor_y -= 1.0;
-            cursor_y = place_new_cursor(&mut stdout, cursor_x, cursor_y, height);
-        }
-    
-        else if key == 2 {
-            remove_old_cursor(&mut stdout, cursor_x, cursor_y);
-            cursor_y += 1.0;
-            cursor_y = place_new_cursor(&mut stdout, cursor_x, cursor_y, height);
-        }
-    
-        else if key == 3 {
-            remove_old_cursor(&mut stdout, cursor_x, cursor_y);
-            cursor_x -= 1.0;
-            cursor_y =  place_new_cursor(&mut stdout, cursor_x, cursor_y, height);
-        }
-    
-        else if key == 4 {
-            remove_old_cursor(&mut stdout, cursor_x, cursor_y);
-            cursor_x += 1.0;
-            cursor_y =  place_new_cursor(&mut stdout, cursor_x, cursor_y, height);
-        }
 
-        else if key == 5 { /* FILE */
-            /* will be there soon, hopefully */
-        }
-
-        else if key == 6 { /* HELP */
-            /* will be there soon, hopefully */
-        }
-
-        else if key == 7 { /* PLACE */
-            place_blok(&mut stdout, cursor_x, cursor_y, 2); /* all colors will be implemented soon, hopefully */
-            cursor_x -= 1.0;
-            cursor_y =  place_new_cursor(&mut stdout, cursor_x, cursor_y, height);
-        }
-
-        else if key == 8 { /* ERASE */
-            place_blok(&mut stdout, cursor_x, cursor_y, 11);
-            cursor_x -= 1.0;
-            cursor_y = place_new_cursor(&mut stdout, cursor_x, cursor_y, height);
-        }
-
-        else if key == 9 { /* QUIT */
-            break;
-        }
-    
-        else {
-            /* ignore */
+        match key {
+            1 => {remove_old_cursor(&mut stdout, cursor_x, cursor_y, &mut placed);
+                    cursor_y -= 1.0;
+                    cursor_y = place_new_cursor(&mut stdout, cursor_x, cursor_y, &mut placed, height);},
+            2 => {remove_old_cursor(&mut stdout, cursor_x, cursor_y, &mut placed);
+                    cursor_y += 1.0;
+                    cursor_y = place_new_cursor(&mut stdout, cursor_x, cursor_y, &mut placed, height);},
+            3 => {remove_old_cursor(&mut stdout, cursor_x, cursor_y, &mut placed);
+                    cursor_x -= 1.0;
+                    cursor_y =  place_new_cursor(&mut stdout, cursor_x, cursor_y, &mut placed, height);},
+            4 => {remove_old_cursor(&mut stdout, cursor_x, cursor_y, &mut placed);
+                    cursor_x += 1.0;
+                    cursor_y =  place_new_cursor(&mut stdout, cursor_x, cursor_y, &mut placed, height);},
+            5 => { /* will be there soon, hopefully */ },
+            6 => { /* will be there soon, hopefully */ },
+            7 => {place_blok(&mut stdout, cursor_x, cursor_y, &mut placed, 2); /* all colors will be implemented soon, hopefully */
+                    cursor_x -= 1.0;
+                    cursor_y =  place_new_cursor(&mut stdout, cursor_x, cursor_y, &mut placed, height);},
+            8 => {place_blok(&mut stdout, cursor_x, cursor_y, &mut placed, 11);
+                    cursor_x -= 1.0;
+                    cursor_y = place_new_cursor(&mut stdout, cursor_x, cursor_y, &mut placed, height);},
+            9 => { break; },
+            _ => { /* ignore */ }
         }
     }
 }
